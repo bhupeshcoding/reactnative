c7f7d65ed71fc19663d14549e37e73f39658e109
bhupeshcoding <130831575+bhupeshcoding@users.noreply.github.com>
2024-02-18 18:23:53 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,101 +1,4 @@
 
-// import { useState } from 'react';
-// import {
-//   StyleSheet,
-//   Text,
-//   View,
-//   Button,
-//   TextInput,
-//   Image,  // Don't forget to import Image component
-//   ScrollView,
-//   FlatList,
-// } from 'react-native';
-
-// export default function App() {
-//   const [enteredGoalText, setEnteredGoalText] = useState('');
-//   const [courseGoals, setCourseGoals] = useState([]);
-
-//   function goalInputHandler(enteredText) {
-//     setEnteredGoalText(enteredText);
-//   }
-
-//   function addGoalHandler() {
-//     setCourseGoals((currentCourseGoals) => [
-//       ...currentCourseGoals,
-//       { text: enteredGoalText, id: Math.random().toString(), image: require('./assets/icon.png') },
-//     ]);
-//   }
-
-//   return (
-//     <View style={styles.appContainer}>
-//       <View style={styles.inputContainer}>
-//         <TextInput
-//           style={styles.textInput}
-//           placeholder="Your course goal!"
-//           onChangeText={goalInputHandler}
-//         />
-//         <Button title="Add Goal" onPress={addGoalHandler} />
-//       </View>
-//       <View style={styles.goalsContainer}>
-//         <FlatList
-//           data={courseGoals}
-//           renderItem={(itemData) => (
-//             <View style={styles.goalItem}>
-//               <Image source={itemData.item.image} style={styles.goalImage} />
-//               <Text style={styles.goalText}>{itemData.item.text}</Text>
-//             </View>
-//           )}
-//           keyExtractor={(item, index) => item.id}
-//           alwaysBounceVertical={false}
-//         />
-//       </View>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   appContainer: {
-//     flex: 1,
-//     paddingTop: 50,
-//     paddingHorizontal: 16,
-//   },
-//   inputContainer: {
-//     flex: 1,
-//     flexDirection: 'row',
-//     justifyContent: 'space-between',
-//     alignItems: 'center',
-//     marginBottom: 24,
-//     borderBottomWidth: 1,
-//     borderBottomColor: '#cccccc',
-//   },
-//   textInput: {
-//     borderWidth: 1,
-//     borderColor: '#cccccc',
-//     width: '70%',
-//     marginRight: 8,
-//     padding: 8,
-//   },
-//   goalsContainer: {
-//     flex: 5,
-//   },
-//   goalItem: {
-//     margin: 8,
-//     padding: 8,
-//     borderRadius: 6,
-//     backgroundColor: '#5e0acc',
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//   },
-//   goalText: {
-//     color: 'white',
-//     marginLeft: 8,
-//   },
-//   goalImage: {
-//     width: 50, // Adjust width and height as needed
-//     height: 50,
-//     marginRight: 8,
-//   },
-// });
 
 import { useState } from 'react';
 import {
@@ -108,6 +11,7 @@ import {
   ScrollView,
   FlatList,
 } from 'react-native';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -148,14 +52,18 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => (
-            <View style={styles.goalItem}>
-              <Image source={itemData.item.image} style={styles.goalImage} />
-              <Text style={styles.goalText}>{itemData.item.text}</Text>
-            </View>
-          )}
-          keyExtractor={(item, index) => item.id}
-          alwaysBounceVertical={false}
-        />
+            <GoalItem
+            goal={itemData.item}
+            style={{
+              goalItem: styles.goalItem,
+              goalText: styles.goalText,
+              goalImage: styles.goalImage,
+            }}
+          />
+        )}
+        keyExtractor={(item, index) => item.id}
+        alwaysBounceVertical={false}
+      />
       </View>
     </View>
   );
